88a3a70446aacc593176b8a36e1ec120b93ddf02
Arnold <ArnoldBustamante@Gmail.com>
2020-05-26 13:49:44 -0400

    first try at menu

---
+++ scripts/sidenav.js
@@ -0,0 +1,9 @@
+// 
+// function openSlideMenu() {
+//   document.getElementById('side-menu').style.width = "200px";
+//   document.getElementById('main').style.margin = "400px";
+// }
+//
+// #btn-hamburger.onclick= openSlideMenu()
+//
+// #btn-close.onclick= closeSlideMenu()
