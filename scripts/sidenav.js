e2953be20a50abbdf9c2a5a592cdf68dabef4f6d
Arnold <ArnoldBustamante@Gmail.com>
2020-05-27 13:11:49 -0400

    scripts/sidenav.js

--- scripts/sidenav.js
+++ scripts/sidenav.js
@@ -3,9 +3,9 @@ const navSlide = () => {
   const nav = document.querySelector ('.navbar');
   const header = document.querySelector ('header');
 
+
   burger.addEventListener('click', () => {
     nav.classList.toggle('nav-active');
-    navLink.classList.toggle('nav-active');
     header.classList.toggle('header-active');
     burger.classList.toggle('toggle');
   });
