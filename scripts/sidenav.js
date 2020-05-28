d50f1fec176a267859ba058216a5db7953ad1436
Arnold <ArnoldBustamante@Gmail.com>
2020-05-28 12:21:30 -0400

    fixed resposive nav bar

--- scripts/sidenav.js
+++ scripts/sidenav.js
@@ -1,15 +1,54 @@
-const navSlide = () => {
-  const burger = document.querySelector ('.burger');
-  const nav = document.querySelector ('.navbar');
-  const header = document.querySelector ('header');
 
+const isMobile = function (){
 
-  burger.addEventListener('click', () => {
-    nav.classList.toggle('nav-active');
-    header.classList.toggle('header-active');
-    burger.classList.toggle('toggle');
-  });
+if (screen.width <= 568) {
+
+  const navSlide = () => {
+
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.navbar');
+    const header = document.querySelector('header');
+    const logo = document.querySelector('.logo');
+    const navlink = document.querySelector('.contact');
+    const project = document.querySelector('.project');
+
+    burger.addEventListener('click', () => {
+      nav.classList.toggle('nav-active');
+      header.classList.toggle('header-active');
+      burger.classList.toggle('toggle');
+      logo.classList.toggle('logo-active');
+    });
+
+    navlink.addEventListener('click', () => {
+      nav.classList.toggle('nav-active');
+      header.classList.toggle('header-active');
+      burger.classList.toggle('toggle');
+      logo.classList.toggle('logo-active');
+    });
+
+    project.addEventListener('click', () => {
+      nav.classList.toggle('nav-active');
+      header.classList.toggle('header-active');
+      burger.classList.toggle('toggle');
+      logo.classList.toggle('logo-active');
+    });
+
+  }
+
+  navSlide();
 
 }
+};
 
-navSlide();
+isMobile();
+
+const windowResize = function (){
+
+
+window.addEventListener('resize', () => {
+  location.reload();
+});
+
+};
+
+windowResize();
