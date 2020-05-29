06842d6be58bd1aeda4ac4b32a7d8c671d94167b
Arnold <ArnoldBustamante@Gmail.com>
2020-05-29 14:10:32 -0400

    added scroll and styled links

--- scripts/nav.js
+++ scripts/nav.js
@@ -9,7 +9,6 @@ $(window).resize(function() {
 
       const burger = document.querySelector('.burger');
       const nav = document.querySelector('.navbar');
-      const header = document.querySelector('header');
       const logo = document.querySelector('.logo');
       const navlink = document.querySelector('.contact');
       const project = document.querySelector('.project');
@@ -17,7 +16,6 @@ $(window).resize(function() {
 
       burger.addEventListener('click', () => {
         nav.classList.toggle('nav-active');
-        header.classList.toggle('header-active');
         burger.classList.toggle('toggle');
         logo.classList.toggle('logo-active');
         body.classList.toggle('body-active');
@@ -25,7 +23,6 @@ $(window).resize(function() {
 
       navlink.addEventListener('click', () => {
         nav.classList.toggle('nav-active');
-        header.classList.toggle('header-active');
         burger.classList.toggle('toggle');
         logo.classList.toggle('logo-active');
         body.classList.toggle('body-active');
@@ -33,7 +30,6 @@ $(window).resize(function() {
 
       project.addEventListener('click', () => {
         nav.classList.toggle('nav-active');
-        header.classList.toggle('header-active');
         burger.classList.toggle('toggle');
         logo.classList.toggle('logo-active');
         body.classList.toggle('body-active');
@@ -44,3 +40,39 @@ $(window).resize(function() {
     navSlide();
 
   };
+
+
+//scroll top
+
+  var lastScroll = 0;
+  var scrollUpStart = 0;
+  $(window).on('scroll',function() {
+
+      var scroll = $(window).scrollTop();
+      if(scroll === 0){
+          // we are at the top
+          $("header").removeClass("header-scroll");
+      } else if(lastScroll > scroll) {
+          // we are scrolling up
+
+          // we check if we have started scrolling up already
+          if (scrollUpStart < scroll) {
+              // if we just started scrolling up, we set
+              // the scrollUpStart to the current scroll value
+              scrollUpStart = scroll;
+          }
+
+          // if we continue scrolling up, the difference between
+          // scrollUpStart and scroll will eventually reach a 100px
+          if (scrollUpStart - scroll > 100) {
+              $("header").addClass("header-scroll");
+          }
+
+      } else {
+          // we are scrolling down
+          // reset scrollUpStart
+          scrollUpStart = 0;
+          $("header").removeClass("header-scroll");
+      }
+      lastScroll = scroll;
+  });
